8eb2b0c4dd1354510e334335ddc4696eef3bd654
wreulicke <saito.masaya@classmethod.jp>
2018-11-21 03:32:27 +0900

    update aws sdk

--- spar-wings-sqs-poller/src/main/java/jp/xet/sparwings/aws/sqs/SqsMessagePoller.java
+++ spar-wings-sqs-poller/src/main/java/jp/xet/sparwings/aws/sqs/SqsMessagePoller.java
@@ -35,17 +35,17 @@ import org.springframework.retry.support.RetryTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.util.DigestUtils;
 
-import com.amazonaws.services.sqs.AmazonSQS;
-import com.amazonaws.services.sqs.model.ChangeMessageVisibilityRequest;
-import com.amazonaws.services.sqs.model.DeleteMessageRequest;
-import com.amazonaws.services.sqs.model.Message;
-import com.amazonaws.services.sqs.model.OverLimitException;
-import com.amazonaws.services.sqs.model.ReceiveMessageRequest;
-import com.amazonaws.services.sqs.model.ReceiveMessageResult;
+import software.amazon.awssdk.services.sqs.SqsClient;
+import software.amazon.awssdk.services.sqs.model.ChangeMessageVisibilityRequest;
+import software.amazon.awssdk.services.sqs.model.DeleteMessageRequest;
+import software.amazon.awssdk.services.sqs.model.Message;
+import software.amazon.awssdk.services.sqs.model.OverLimitException;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageRequest;
+import software.amazon.awssdk.services.sqs.model.ReceiveMessageResponse;
 
 /**
  * TODO for daisuke
- * 
+ *
  * @since 0.3
  * @version $Id$
  * @author daisuke
@@ -55,7 +55,7 @@ import com.amazonaws.services.sqs.model.ReceiveMessageResult;
 public class SqsMessagePoller { // NOPMD - cc
 	
 	@Getter
-	private final AmazonSQS sqs;
+	private final SqsClient sqs;
 	
 	@Getter
 	private final RetryTemplate retry;
@@ -97,7 +97,7 @@ public class SqsMessagePoller { // NOPMD - cc
 	
 	/**
 	 * TODO for daisuke
-	 * 
+	 *
 	 * @since 0.3
 	 */
 	@Scheduled(fixedDelay = 1) // SUPPRESS CHECKSTYLE bug?
@@ -112,15 +112,18 @@ public class SqsMessagePoller { // NOPMD - cc
 	}
 	
 	private List<Message> reveiveMessages() {
-		ReceiveMessageResult receiveMessageResult;
+		ReceiveMessageResponse receiveMessageResult;
 		try {
 			log.trace("Start SQS long polling");
-			receiveMessageResult = sqs.receiveMessage(new ReceiveMessageRequest(workerQueueUrl)
-				.withWaitTimeSeconds(waitTimeSeconds)
-				.withMaxNumberOfMessages(maxNumberOfMessages)
-				.withVisibilityTimeout(visibilityTimeout)
-				.withAttributeNames("ApproximateReceiveCount"));
-			return receiveMessageResult.getMessages();
+			receiveMessageResult = sqs.receiveMessage(ReceiveMessageRequest
+				.builder()
+				.queueUrl(workerQueueUrl)
+				.waitTimeSeconds(waitTimeSeconds)
+				.maxNumberOfMessages(maxNumberOfMessages)
+				.visibilityTimeout(visibilityTimeout)
+				.attributeNamesWithStrings("ApproximateReceiveCount")
+				.build());
+			return receiveMessageResult.messages();
 		} catch (OverLimitException e) {
 			log.error("SQS over limit", e);
 			try {
@@ -134,32 +137,35 @@ public class SqsMessagePoller { // NOPMD - cc
 	}
 	
 	private void handleMessage(Message message) {
-		log.info("SQS message was recieved: {}", message.getMessageId());
+		log.info("SQS message was recieved: {}", message.messageId());
 		log.debug("Receive SQS:{} C:{} RHD:{}",
-				message.getMessageId(),
-				message.getAttributes().get("ApproximateReceiveCount"),
+				message.messageId(),
+				message.attributesAsStrings().get("ApproximateReceiveCount"),
 				computeReceptHandleDigest(message));
 		
 		Future<Message> future = executor.submit(() -> messageHandler.accept(message), message);
-		log.debug("Main task for {} is submitted", message.getMessageId());
+		log.debug("Main task for {} is submitted", message.messageId());
 		
 		doFollowup(message, future);
 	}
 	
 	private void doFollowup(Message message, Future<Message> future) {
-		log.debug("Start visibility timeout follow-up task for {}", message.getMessageId());
+		log.debug("Start visibility timeout follow-up task for {}", message.messageId());
 		try {
 			retry.execute(context -> {
 				try {
 					future.get(changeVisibilityThreshold, TimeUnit.SECONDS);
-					log.debug("Job for SQS:{} was done", message.getMessageId());
-					sqs.deleteMessage(new DeleteMessageRequest(workerQueueUrl, message.getReceiptHandle()));
-					log.info("SQS:{} was deleted", message.getMessageId());
+					log.debug("Job for SQS:{} was done", message.messageId());
+					sqs.deleteMessage(DeleteMessageRequest.builder()
+						.queueUrl(workerQueueUrl)
+						.receiptHandle(message.receiptHandle())
+						.build());
+					log.info("SQS:{} was deleted", message.messageId());
 				} catch (InterruptedException e) {
 					Thread.currentThread().interrupt();
-					log.warn("Job for SQS:{} was interrupted", message.getMessageId());
+					log.warn("Job for SQS:{} was interrupted", message.messageId());
 				} catch (ExecutionException e) { // handle e.getCause()
-					log.error("Job for SQS:{} was failed", message.getMessageId(), e.getCause());
+					log.error("Job for SQS:{} was failed", message.messageId(), e.getCause());
 				} catch (TimeoutException e) { // we need more time
 					extendTimeout(message);
 					throw e;
@@ -169,18 +175,21 @@ public class SqsMessagePoller { // NOPMD - cc
 		} catch (Exception e) { // NOPMD - cc
 			log.error("Retry attempt exceeded?", e);
 		}
-		log.debug("Visibility timeout follow-up task for {} was finished", message.getMessageId());
+		log.debug("Visibility timeout follow-up task for {} was finished", message.messageId());
 	}
 	
 	private void extendTimeout(Message message) {
-		log.debug("Job for SQS:{} was timeout RHD:{}", message.getMessageId(), computeReceptHandleDigest(message));
-		sqs.changeMessageVisibility(new ChangeMessageVisibilityRequest(
-				workerQueueUrl, message.getReceiptHandle(), visibilityTimeout));
+		log.debug("Job for SQS:{} was timeout RHD:{}", message.messageId(), computeReceptHandleDigest(message));
+		sqs.changeMessageVisibility(ChangeMessageVisibilityRequest.builder()
+			.queueUrl(workerQueueUrl)
+			.receiptHandle(message.receiptHandle())
+			.visibilityTimeout(visibilityTimeout)
+			.build());
 		if (log.isDebugEnabled()) {
-			log.debug("Visibility for SQS:{} was updated VT:{}", message.getMessageId(), visibilityTimeout);
+			log.debug("Visibility for SQS:{} was updated VT:{}", message.messageId(), visibilityTimeout);
 		} else if (log.isTraceEnabled()) {
 			log.trace("Visibility for SQS:{} was updated VT:{} RHD:{}",
-					message.getMessageId(),
+					message.messageId(),
 					visibilityTimeout,
 					computeReceptHandleDigest(message));
 		}
@@ -191,7 +200,7 @@ public class SqsMessagePoller { // NOPMD - cc
 			
 			@Override
 			public String toString() {
-				return DigestUtils.md5DigestAsHex(message.getReceiptHandle().getBytes(StandardCharsets.UTF_8));
+				return DigestUtils.md5DigestAsHex(message.receiptHandle().getBytes(StandardCharsets.UTF_8));
 			}
 		};
 	}
